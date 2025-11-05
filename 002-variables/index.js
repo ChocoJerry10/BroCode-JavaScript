da27746ebb1c718c615108ad4bd60750a6ee9174
Rijan Bhandari <151892951+ChocoJerry10@users.noreply.github.com>
2025-11-05 23:07:27 +0545

    Add files via upload

---
+++ 002-variables/index.js
@@ -0,0 +1,43 @@
+
+// let age = 23;
+// let price = 10.99;
+// let gpa = 3.7;
+
+// console.log(`you are ${age} years old`);
+// console.log(`the price is ${price}`);
+// console.log(`your gpa is ${gpa}`);
+
+
+// let firstName = "Rijan";
+// let favoriteFood = "Pizza";
+// let email = "bro@gmail.com"
+
+// console.log(typeof firstName);
+// console.log(`your firstName is ${firstName}`);
+// console.log(`your email is ${email}`)
+// console.log(`your favorite food is ${favoriteFood}`)
+
+// let online = true;
+// let forSale = false;
+// let isStudent = true;
+
+// console.log(`bro is online: ${online}`);
+// console.log(`this item is for sale: ${forSale}`); 
+// console.log(`Enrolled: ${isStudent}`);
+
+let fullName = "Rijan Bhandari";
+let age = 23;
+let Student = true;
+
+document.getElementById("p1").textContent= `My name is ${fullName}`;
+document.getElementById("p2").textContent= `I am ${age} years old`;
+document.getElementById("p3").textContent= `Enrolled: ${Student}`;
+
+
+
+
+
+
+
+
+
---
+++ 003-arithmeticOperators/index.js
@@ -0,0 +1,32 @@
+// arithmetic operators = operands (values, variables, etc.) 
+
+// operators ( +, -, *, /, %   )
+// ex: 11 = X + 4;
+
+// let students = 30;
+
+// // students = students + 1 ; 
+// // students = students - 1; 
+// // students = students * 2; 
+// // students = students / 2; 
+// let extraStudents = students % 4;
+
+
+// console.log(students);
+// console.log(extraStudents);
+
+
+
+/* 
+    operators precedence
+    1. Parentheses ()
+    2. Exponents **
+    3. Multiplication * , Division / , Modulus %
+    4. Addition + , Subtraction -
+
+*/
+
+let result = 12 % 5 + 8 / 2; // 12% 5 =2 + 8/2=4 => 2+4=6
+
+
+console.log(result);
